9f4a1d18c99e7e2e485a0776cedfdfd7cb97afdf
Gabriel Augusto <101129486+L4dk3@users.noreply.github.com>
2023-11-05 15:33:19 -0300

    Delete Peixes directory

--- Peixes/src/main/java/gabriel/brito/ufpb/br/PeixeJaExisteException.java
+++
@@ -1,7 +0,0 @@
-package gabriel.brito.ufpb.br;
-
-public class PeixeJaExisteException extends RuntimeException{
-    public PeixeJaExisteException (String  menssage){
-        super(menssage);
-    }
-}
